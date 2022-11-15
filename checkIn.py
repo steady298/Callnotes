cdf9111b166d93f5c376584b5b6dc84003598b01
steady298 <115759238+steady298@users.noreply.github.com>
2022-11-15 17:38:40 +0800

    Update checkIn.py

--- checkIn.py
+++ checkIn.py
@@ -1,4 +1,6 @@
+import os
 import urllib.request
+SERVERPUSHKEY = os.environ["SERVERPUSHKEY"]
 url="https://api2.pushdeer.com/message/push?pushkey=" + SERVERPUSHKEY + "&text=❤️周末到了给父母回个电话吧❤️"
 req=urllib.request.Request(url)
 resp=urllib.request.urlopen(req)
