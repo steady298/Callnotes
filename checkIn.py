e459f43b9728c0b8335b574124a50cd9b6c54f95
steady298 <115759238+steady298@users.noreply.github.com>
2022-11-15 17:22:38 +0800

    Update checkIn.py

--- checkIn.py
+++ checkIn.py
@@ -1,36 +1,6 @@
-# coding=utf-8 
-from selenium import webdriver
-import time
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.support.ui import WebDriverWait
-import configparser
-import os
-import sys, traceback
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
+url="https://api2.pushdeer.com/message/push?pushkey=" + SERVERPUSHKEY + "&text=❤️周末到了给父母回个电话吧❤️"
+req=urllib.request.Request(url)
+resp=urllib.request.urlopen(req)
+data=resp.read().decode('utf-8')
 
-mobile_emulation = {"deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
-                    "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"}
-
-# 配置加载策略
-# desired_capabilities = DesiredCapabilities.CHROME  # 修改页面加载策略
-# desired_capabilities["pageLoadStrategy"] = "eager"  # 注释这两行会导致最后输出结果的延迟，即等待页面加载完成再输出
-chrome_options = Options()  # 实例化一个启动参数对象
-
-chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)  # 这里看清楚了，不是add_argument
-# chrome_options.add_argument('--window-size=1366,1400')  # 设置浏览器窗口大小
-prefs = {"profile.managed_default_content_settings.images": 2, 'permissions.default.stylesheet': 2}  # 禁止加载图片和CSS样式
-chrome_options.add_experimental_option("prefs", prefs)
-
-chrome_options.add_argument('window-size=1024,768')  # 16年之后，chrome给出的解决办法，抢了PhantomJS饭碗
-chrome_options.add_argument('--headless')  # 16年之后，chrome给出的解决办法，抢了PhantomJS饭碗
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')  # root用户不加这条会无法运行
-
-
-driver = webdriver.Chrome(options=chrome_options)  # 获取浏览器句柄
-        url = "https://api2.pushdeer.com/message/push?pushkey=" + SERVERPUSHKEY + "&text=❤️周末到了给父母回个电话吧❤️"
-        driver.get(url)
-
-SERVERPUSHKEY = os.environ["SERVERPUSHKEY"]
+print(data
